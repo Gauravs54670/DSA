6dc7ac8cb1aac04362f30a9fd78903980cbac480
Gaurav <gauravsoni54670@gmail.com>
2026-03-11 23:54:24 +0530

    Remove Duplicate from Array

---
+++ Arrays/RemoveDuplicate.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveDuplicate {
+    private static List<Integer> removeDuplicate(int arr[]) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0,j = 1;
+        if(arr.length == 1) {
+            list.add(arr[i]);
+            return list;
+        }
+        while(j < arr.length) {
+            if(arr[j] != arr[i]){
+                list.add(arr[j]);
+                i = j;
+                j++;
+            }
+            else j++;
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,5,8,8,9,9,16,16};
+        System.out.println("Removed Duplicates list " + removeDuplicate(arr));
+    }
+}
